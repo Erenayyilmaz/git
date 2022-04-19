3db175974db4dbc8d378323e231465a6a2737f30
Erenayyilmaz <74668808+Erenayyilmaz@users.noreply.github.com>
2022-04-19 19:28:13 +0300

    1

---
+++ a.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void fonk(int a()){
+    printf("asd");
+
+
+}
+
+int a(){
+    return 4;
+}
+
+int as(int a,int b,char c){
+    return 9;
+}
+
+int main(void){
+
+    int a=7,b =6;
+    char c='f';
+
+
+    int* x(int,int,char);
+
+    x= &as(a,b ,c);
+}
